855105b0027bb65df8ba7469121b9f032ee22365
aphaphon <pui.aphaphon@gmail.com>
2018-11-07 18:23:31 +0700

    Update readme & source code.

--- src/CodeBasic/Pokdeng.cs
+++ src/CodeBasic/Pokdeng.cs
@@ -6,13 +6,14 @@ namespace CodeBasic
 {
     public class Pokdeng
     {
-        public int Player1Balance { get; set; }
-        public int Player2Balance { get; set; }
+        public int PlayerBalance { get; set; }
 
+        // Club, Diamond, Heart, Spade (case sensitive)
         public void CheckGameResult(
-            int p1BetAmount, int p1CardNo1, int p1CardNo2, int p1CardNo3, 
+            int betAmount,
+            int p1CardNo1, int p1CardNo2, int p1CardNo3, 
             string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3,
-            int p2BetAmount, int p2CardNo1, int p2CardNo2, int p2CardNo3,
+            int p2CardNo1, int p2CardNo2, int p2CardNo3,
             string p2CardSymbol1, string p2CardSymbol2, string p2CardSymbol3)
         {
             throw new NotImplementedException();
