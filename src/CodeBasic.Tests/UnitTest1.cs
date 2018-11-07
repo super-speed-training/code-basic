18cc43919e2c2a5cf9f00dfb46ed654b66bb9d94
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2018-11-07 15:18:20 +0700

    Update Pokdeng source code.

--- src/CodeBasic.Tests/UnitTest1.cs
+++ src/CodeBasic.Tests/UnitTest1.cs
@@ -5,6 +5,10 @@ namespace CodeBasic.Tests
 {
     public class UnitTest1
     {
+        // Normal cases
+        // Alternative cases
+        // Exception cases
+
         [Fact]
         public void Test1()
         {
---
+++ src/CodeBasic/Pokdeng.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeBasic
+{
+    public class Pokdeng
+    {
+        public int Player1Balance { get; set; }
+        public int Player2Balance { get; set; }
+
+        public void CheckGameResult(
+            int p1BetAmount, int p1CardNo1, int p1CardNo2, int p1CardNo3, 
+            string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3,
+            int p2BetAmount, int p2CardNo1, int p2CardNo2, int p2CardNo3,
+            string p2CardSymbol1, string p2CardSymbol2, string p2CardSymbol3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
