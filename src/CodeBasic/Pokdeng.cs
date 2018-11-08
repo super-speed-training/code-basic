fa6aa2b84b1c9f6c295ca17d49218e534abecf81
woraput <woraput@kkumail.com>
2018-11-08 14:40:31 +0700

    setup Quiz follow P.Au

--- src/CodeBasic/Pokdeng.cs
+++ src/CodeBasic/Pokdeng.cs
@@ -11,12 +11,26 @@ namespace CodeBasic
         // Club, Diamond, Heart, Spade (case sensitive)
         public void CheckGameResult(
             int betAmount,
-            int p1CardNo1, int p1CardNo2, int p1CardNo3, 
+            int p1CardNo1, int p1CardNo2, int p1CardNo3,
             string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3,
             int p2CardNo1, int p2CardNo2, int p2CardNo3,
             string p2CardSymbol1, string p2CardSymbol2, string p2CardSymbol3)
         {
-            throw new NotImplementedException();
+            var dealerPoints = p1CardNo1 + p1CardNo2 + p1CardNo3;
+            var playerPoints = p2CardNo1 + p2CardNo2 + p2CardNo3;
+
+            var isGameDraw = dealerPoints == playerPoints;
+            if (isGameDraw) return;
+
+            var isPlayerTheWinner = playerPoints > dealerPoints;
+            if (isPlayerTheWinner)
+            {
+                PlayerBalance += betAmount;
+            }
+            else
+            {
+                PlayerBalance -= betAmount;
+            }
         }
     }
 }
