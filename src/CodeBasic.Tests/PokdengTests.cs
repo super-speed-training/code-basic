84db48364a4fa499a33ada3352e8f94a8e8d4177
woraput <woraput@kkumail.com>
2018-11-08 19:57:45 +0700

    add test case PlayerDrawX2ThenDoNothing

--- src/CodeBasic.Tests/PokdengTests.cs
+++ src/CodeBasic.Tests/PokdengTests.cs
@@ -70,6 +70,18 @@ namespace CodeBasic.Tests
             sut.CheckGameResult(bet, p1cn1, p1cn2, 0, p1cs1, p1cs2, string.Empty, p2cn1, p2cn2, 0, p2cs1, p2cs2, string.Empty);
             Assert.Equal(expectedBalance, sut.PlayerBalance);
         }
+
+        [Theory(DisplayName = "แต้มผู้เล่นแพ้เจ้ามือ2เด้ง ผู้เล่นผู้เล่นเสียเงิน2เท่าของเงินที่ลงพนัน")]
+        [InlineData(100, 1, 5, Heart, Heart, 4, 2, Club, Club, 1000, 1000)]
+        [InlineData(200, 1, 6, Heart, Heart, 4, 3, Club, Club, 1000, 1000)]
+        [InlineData(300, 1, 4, Heart, Heart, 3, 2, Club, Club, 1000, 1000)]
+        public void PlayerDrawX2ThenDoNothing(int bet, int p1cn1, int p1cn2, string p1cs1, string p1cs2, int p2cn1, int p2cn2, string p2cs1, string p2cs2, int balance, int expectedBalance)
+        {
+            var sut = new Pokdeng { PlayerBalance = balance };
+            sut.PlayerBalance = balance;
+            sut.CheckGameResult(bet, p1cn1, p1cn2, 0, p1cs1, p1cs2, string.Empty, p2cn1, p2cn2, 0, p2cs1, p2cs2, string.Empty);
+            Assert.Equal(expectedBalance, sut.PlayerBalance);
+        }
         /*
          * Normal cases
          * แต้มผู้เล่นชนะเจ้ามือ โดยผลรวมเกิน 9 ผู้เล่นได้รับเงินเพิ่มเท่ากับเงินที่ลงพนัน
