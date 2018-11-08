0286f10d488bde75c32b61d157b9ee8cdd67314c
parada <parada@perfenterprise.com>
2018-11-08 14:40:16 +0700

    Add GetMultipleResultTypeTest.

--- src/CodeBasic.Tests/PokdengTest.cs
+++ src/CodeBasic.Tests/PokdengTest.cs
@@ -37,12 +37,28 @@ namespace CodeBasic.Tests
         [InlineData(PokdengInfo.PlayerResult.Ghost, 11, 12, 13, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Spade)]
         [InlineData(PokdengInfo.PlayerResult.Ghost, 12, 12, 13, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Spade)]
         [InlineData(PokdengInfo.PlayerResult.Set, 1, 2, 3, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Spade)]
-        [InlineData(PokdengInfo.PlayerResult.Set, 8, 9, 10, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Spade)]
+        [InlineData(PokdengInfo.PlayerResult.Set, 9, 10, 8, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Spade)]
         [InlineData(PokdengInfo.PlayerResult.Twobounce, 2, 2, 0, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Diamond, "")]
         [InlineData(PokdengInfo.PlayerResult.Twobounce, 1, 2, 0, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Heart, "")]
         [InlineData(PokdengInfo.PlayerResult.ThreeBounce, 2, 2, 2, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Spade)]
         [InlineData(PokdengInfo.PlayerResult.ThreeBounce, 1, 2, 5, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Heart)]
-        public void GetRewardTypeTest(PokdengInfo.PlayerResult expected, int p1CardNo1, int p1CardNo2, int p1CardNo3, string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3)
+        public void GetResultTypeTest(PokdengInfo.PlayerResult expected, int p1CardNo1, int p1CardNo2, int p1CardNo3, string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3)
+        {
+            var player = new Pokdeng();
+            var result = player.GetRewardType(p1CardNo1, p1CardNo2, p1CardNo3, p1CardSymbol1, p1CardSymbol2, p1CardSymbol3);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory(DisplayName = "ดึงผลลัพธ์จากไพ่ของผู้เล่นกรณีมีหลายผลลัพธ์")]
+        [InlineData(PokdengInfo.PlayerResult.Pok8Twobounce, 10, 8, 0, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Heart, "")]
+        [InlineData(PokdengInfo.PlayerResult.Pok8Twobounce, 5, 3, 0, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Heart, "")]
+        [InlineData(PokdengInfo.PlayerResult.Pok9Twobounce, 8, 1, 0, PokdengInfo.Symbol.Club, PokdengInfo.Symbol.Club, "")]
+        [InlineData(PokdengInfo.PlayerResult.Pok9Twobounce, 10, 9, 0, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Diamond, "")]
+        [InlineData(PokdengInfo.PlayerResult.SetThreeBounce, 2, 4, 3, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Diamond)]
+        [InlineData(PokdengInfo.PlayerResult.SetThreeBounce, 4, 5, 6, PokdengInfo.Symbol.Spade, PokdengInfo.Symbol.Spade, PokdengInfo.Symbol.Spade)]
+        [InlineData(PokdengInfo.PlayerResult.GhostThreeBounce, 11, 12, 13, PokdengInfo.Symbol.Spade, PokdengInfo.Symbol.Spade, PokdengInfo.Symbol.Spade)]
+        [InlineData(PokdengInfo.PlayerResult.GhostThreeBounce, 12, 13, 11, PokdengInfo.Symbol.Spade, PokdengInfo.Symbol.Spade, PokdengInfo.Symbol.Spade)]
+        public void GetMultipleResultTypeTest(PokdengInfo.PlayerResult expected, int p1CardNo1, int p1CardNo2, int p1CardNo3, string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3)
         {
             var player = new Pokdeng();
             var result = player.GetRewardType(p1CardNo1, p1CardNo2, p1CardNo3, p1CardSymbol1, p1CardSymbol2, p1CardSymbol3);
