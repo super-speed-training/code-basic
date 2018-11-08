e9ca4742eb648b85841b29a3c2bfc36ed9efe105
parada <parada@perfenterprise.com>
2018-11-08 10:49:59 +0700

    Set up test.

---
+++ src/CodeBasic.Tests/PokdengTest.cs
@@ -0,0 +1,73 @@
+using System;
+using Xunit;
+
+namespace CodeBasic.Tests
+{
+    public class PokdengTest
+    {
+        // Normal cases
+        // Alternative cases
+        // Exception cases Wrong Card
+
+        [Fact]
+        public void CheckGameResultByNormalCardTest()
+        {
+
+        }
+
+        [Fact]
+        public void CheckGameResultByWrongCardTest()
+        {
+
+        }
+
+        [Fact]
+        public void PlayableTest()
+        {
+
+        }
+
+        [Fact]
+        public void IsPoKTest()
+        {
+
+        }
+
+        [Fact]
+        public void IsPokTwobounce()
+        {
+
+        }
+
+        [Fact]
+        public void IsTongTest()
+        {
+
+        }
+
+        [Fact]
+        public void IsGhostTest()
+        {
+
+        }
+
+        [Fact]
+        public void IsSet()
+        {
+
+        }
+
+        [Fact]
+        public void IsTwobounce()
+        {
+
+        }
+
+        [Fact]
+        public void IsThreeBounce()
+        {
+
+        }
+    }
+
+}
