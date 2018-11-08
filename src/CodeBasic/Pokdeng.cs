ab86e41cce4339decea12e0aac66c673b7a4b825
parada <parada@perfenterprise.com>
2018-11-08 16:07:50 +0700

    Refactor code.

--- src/CodeBasic/Pokdeng.cs
+++ src/CodeBasic/Pokdeng.cs
@@ -111,8 +111,8 @@ namespace CodeBasic
 
         public string GetWinnerBySpecialResult(PokdengInfo.PlayerResult p1Result, PokdengInfo.PlayerResult p2Result)
         {
-            p1Result = (int)p1Result >= 10 ? (PokdengInfo.PlayerResult)((int)p1Result - 10) : p1Result;
-            p2Result = (int)p2Result >= 10 ? (PokdengInfo.PlayerResult)((int)p2Result - 10) : p2Result;
+            p1Result = (int)p1Result >= 10 ? p1Result - 10 : p1Result;
+            p2Result = (int)p2Result >= 10 ? p2Result - 10 : p2Result;
 
             if (p1Result < p2Result)
             {
