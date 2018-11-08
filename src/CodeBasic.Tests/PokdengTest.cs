4bdd0caa29156e20a40ab8148edc614a47c7a6f1
parada <parada@perfenterprise.com>
2018-11-08 13:42:09 +0700

    Implement GetRewardTypeTest

--- src/CodeBasic.Tests/PokdengTest.cs
+++ src/CodeBasic.Tests/PokdengTest.cs
@@ -1,27 +1,16 @@
-using System;
+﻿using System;
 using Xunit;
 
 namespace CodeBasic.Tests
 {
     public class PokdengTest
     {
-        // Normal cases
-        // Alternative cases
-        // Exception cases Wrong Card
+        const string Club = "Club";
+        const string Diamond = "Diamond";
+        const string Heart = "Heart";
+        const string Spade = "Spade";
 
-        [Fact]
-        public void CheckGameResultByNormalCardTest()
-        {
-
-        }
-
-        [Fact]
-        public void CheckGameResultByWrongCardTest()
-        {
-
-        }
-
-        [Theory]
+        [Theory(DisplayName = "ตรสจสอบว่าผู้เล่นสามารถเล่นได้หรือไม่จากจำรวนเงินที่ผู้เล่นเดิมพันกับจำนวนเงินที่ผู้เล่นมี")]
         [InlineData(100, 10, true)]
         [InlineData(500, 100, true)]
         [InlineData(0, 0, false)]
@@ -36,16 +25,28 @@ namespace CodeBasic.Tests
             Assert.Equal(expected, result);
         }
 
-        [Fact]
-        public void GetRewardType()
+        [Theory(DisplayName = "ดึงผลลัพธ์จากไพ่ของผู้เล่นกรณีปกติ")]
+        [InlineData(PokdengInfo.PlayerResult.Normal, 1, 4, 5, PokdengInfo.Symbol.Club, PokdengInfo.Symbol.Spade, PokdengInfo.Symbol.Diamond)]
+        [InlineData(PokdengInfo.PlayerResult.Normal, 10, 11, 5, PokdengInfo.Symbol.Club, PokdengInfo.Symbol.Club, PokdengInfo.Symbol.Diamond)]
+        [InlineData(PokdengInfo.PlayerResult.Pok9, 9, 11, 0, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Club, "")]
+        [InlineData(PokdengInfo.PlayerResult.Pok9, 8, 1, 0, PokdengInfo.Symbol.Club, PokdengInfo.Symbol.Diamond, "")]
+        [InlineData(PokdengInfo.PlayerResult.Pok8, 10, 8, 0, PokdengInfo.Symbol.Spade, PokdengInfo.Symbol.Diamond, "")]
+        [InlineData(PokdengInfo.PlayerResult.Pok8, 2, 6, 0, PokdengInfo.Symbol.Spade, PokdengInfo.Symbol.Heart, "")]
+        [InlineData(PokdengInfo.PlayerResult.Tong, 3, 3, 3, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Spade)]
+        [InlineData(PokdengInfo.PlayerResult.Tong, 11, 11, 11, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Spade)]
+        [InlineData(PokdengInfo.PlayerResult.Ghost, 11, 12, 13, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Spade)]
+        [InlineData(PokdengInfo.PlayerResult.Ghost, 12, 12, 13, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Spade)]
+        [InlineData(PokdengInfo.PlayerResult.Set, 1, 2, 3, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Spade)]
+        [InlineData(PokdengInfo.PlayerResult.Set, 8, 9, 10, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Diamond, PokdengInfo.Symbol.Spade)]
+        [InlineData(PokdengInfo.PlayerResult.Twobounce, 2, 2, 0, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Diamond, "")]
+        [InlineData(PokdengInfo.PlayerResult.Twobounce, 1, 2, 0, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Heart, "")]
+        [InlineData(PokdengInfo.PlayerResult.ThreeBounce, 2, 2, 2, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Spade)]
+        [InlineData(PokdengInfo.PlayerResult.ThreeBounce, 1, 2, 5, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Heart, PokdengInfo.Symbol.Heart)]
+        public void GetRewardTypeTest(PokdengInfo.PlayerResult expected, int p1CardNo1, int p1CardNo2, int p1CardNo3, string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3)
         {
-
-        }
-
-        [Fact]
-        public void GetRewardTest()
-        {
-
+            var player = new Pokdeng();
+            var result = player.GetRewardType(p1CardNo1, p1CardNo2, p1CardNo3, p1CardSymbol1, p1CardSymbol2, p1CardSymbol3);
+            Assert.Equal(expected, result);
         }
     }
 }
