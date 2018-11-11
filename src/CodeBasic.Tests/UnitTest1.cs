bebbe575bd6829d437fdb33738d43f80dbe973ef
tatae <tachapong999.gmail.com>
2018-11-11 15:46:26 +0700

    Test SumCardP1PokWin Pokdeng

--- src/CodeBasic.Tests/UnitTest1.cs
+++ src/CodeBasic.Tests/UnitTest1.cs
@@ -9,9 +9,25 @@ namespace CodeBasic.Tests
         // Alternative cases
         // Exception cases
 
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(100,4,5,3,4,1000,1100)]
+        [InlineData(100,6,3,7,1,1000,1200)]
+        //  [InlineData(100,4,4,3,4,1000,1100)]
+        // [InlineData(100,9,9,7,1,1000,1200)]
+
+
+        public void SumCardP1PokWin (int betAmount,int p1CardNo1, int p1CardNo2,
+         int p2CardNo1, int p2CardNo2,int Balance, int excepted)
         {
+           var a = new Pokdeng();
+           a.PlayerBalance = Balance;
+           a.CheckGameResult(
+             betAmount,
+             p1CardNo1, p1CardNo2, 0,
+            string.Empty, string.Empty, string.Empty ,
+             p2CardNo1, p2CardNo2, 0,
+            string.Empty, string.Empty, string.Empty);
+            
 
         }
     }
--- src/CodeBasic/Pokdeng.cs
+++ src/CodeBasic/Pokdeng.cs
@@ -7,16 +7,24 @@ namespace CodeBasic
     public class Pokdeng
     {
         public int PlayerBalance { get; set; }
+        
 
         // Club, Diamond, Heart, Spade (case sensitive)
-        public void CheckGameResult(
+        public int CheckGameResult(
             int betAmount,
-            int p1CardNo1, int p1CardNo2, int p1CardNo3, 
+            int p1CardNo1, int p1CardNo2, int p1CardNo3,
             string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3,
             int p2CardNo1, int p2CardNo2, int p2CardNo3,
             string p2CardSymbol1, string p2CardSymbol2, string p2CardSymbol3)
         {
-            throw new NotImplementedException();
+            int SumCardP1 = p1CardNo1+p1CardNo2;
+            int SumCardP2 = p2CardNo1+p2CardNo2;
+
+            if(SumCardP1 == 8 || SumCardP1 == 9 && SumCardP1 > SumCardP2)
+            {
+                return PlayerBalance+betAmount;
+            }
+           return 0;
         }
     }
 }
