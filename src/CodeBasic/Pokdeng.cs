06784b498ef9ff10e0e43c44541fab2d2ab17382
Sikarin <so.sikarin@perenterprise.com>
2018-11-11 15:44:44 +0700

    CreateCards

--- src/CodeBasic/Pokdeng.cs
+++ src/CodeBasic/Pokdeng.cs
@@ -11,12 +11,66 @@ namespace CodeBasic
         // Club, Diamond, Heart, Spade (case sensitive)
         public void CheckGameResult(
             int betAmount,
-            int p1CardNo1, int p1CardNo2, int p1CardNo3, 
+            int p1CardNo1, int p1CardNo2, int p1CardNo3,
             string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3,
             int p2CardNo1, int p2CardNo2, int p2CardNo3,
             string p2CardSymbol1, string p2CardSymbol2, string p2CardSymbol3)
         {
-            throw new NotImplementedException();
+            var Banker = CreateCards(p1CardNo1, p1CardNo2, p1CardNo3, p1CardSymbol1, p1CardSymbol2, p1CardSymbol3);
+            var Player = CreateCards(p2CardNo1, p2CardNo2, p2CardNo3, p2CardSymbol1, p2CardSymbol2, p2CardSymbol3);
+        }
+
+        public Card[] CreateCards(
+            int cardNo1,
+            int cardNo2,
+            int cardNo3,
+            string cardSymbol1,
+            string cardSymbol2,
+            string cardSymbol3)
+        {
+            var card = new List<Card>();
+            int[] Point = new int[] { cardNo1, cardNo2, cardNo3 };
+            string[] Sym = new String[] { cardSymbol1, cardSymbol2, cardSymbol3 };
+
+            for (int i = 0; i < Point.Length; i++)
+            {
+                if (Point[i] != 0 && Sym[i] != "")
+                {
+                    if (Sym[i] == "Club")
+                    {
+                        card.Add(new Card(Point[i], CardType.Club));
+                    }
+                    else if (Sym[i] == "Diamond")
+                    {
+                        card.Add(new Card(Point[i], CardType.Diamond));
+                    }
+                    else if (Sym[i] == "Heart")
+                    {
+                        card.Add(new Card(Point[i], CardType.Heart));
+                    }
+                    else
+                    {
+                        card.Add(new Card(Point[i], CardType.Spade));
+                    }
+                }
+            }
+            return card.ToArray();
+        }
+
+        public ScoreRank GetRank(Card[] Card)
+        {
+            if (Card.Length == 2 && Card[0].Point + Card[1].Point >= 8)
+            {
+                return ScoreRank.Pok;
+            }
+            if (Card.Length == 3 && Card[0].Point == Card[1].Point && Card[1].Point == Card[2].Point)
+            {
+                return ScoreRank.Three;
+            }
+            if (Card.Length == 3 && Card[0].Point > 10 && Card[1].Point > 10 && Card[2].Point > 10){
+                return ScoreRank.Ghost;
+            }
+            return new ScoreRank();
         }
     }
 }
