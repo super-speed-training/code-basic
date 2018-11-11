74eb6d2665b1aa0565d53f3b58c356a6a7833072
thaninpong <thaninpong@perfenterprise.com>
2018-11-11 19:19:51 +0700

    Implement point pok 2deng  3 deng  and test
    Signed-off-by: thaninpong <thaninpong@perfenterprise.com>

---
+++ src/CodeBasic.Tests/PokdengTest.cs
@@ -0,0 +1,61 @@
+using System;
+using Xunit;
+using static CodeBasic.Pokdeng;
+
+namespace CodeBasic.Tests
+{
+    public class PokdengTest
+    {
+
+        [Theory(DisplayName = "Point")]
+        [InlineData(1, 2, 3, 6)]
+        [InlineData(4, 3, 0, 7)]
+        [InlineData(11, 4, 3, 7)]
+        [InlineData(1, 5, 11, 6)]
+        [InlineData(11, 12, 1, 1)]
+        public void CheckCalculatePointTest(int cardNo1, int cardNo2, int cardNo3, int expected)
+        {
+            var sut = new Pokdeng();
+            var result = sut.SumCard(cardNo1, cardNo2, cardNo3);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory(DisplayName = "Pok")]
+        [InlineData(4, 4, 0, true)]
+        [InlineData(10, 9, 0, true)]
+        [InlineData(8, 1, 0, true)]
+        [InlineData(3, 7, 5, false)]
+        [InlineData(3, 3, 3, false)]
+        public void CheckPokgTest(int cardNo1, int cardNo2, int cardNo3, bool expected)
+        {
+            var sut = new Pokdeng();
+            var result = sut.IsPokCheck(cardNo1, cardNo2, cardNo3);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory(DisplayName = "TwoDeng")]
+        [InlineData(4, 4, Symbol.Club, Symbol.Heart, true)]
+        [InlineData(3, 3, Symbol.Club, Symbol.Heart, true)]
+        [InlineData(10, 9, Symbol.Heart, Symbol.Heart, true)]
+        [InlineData(1, 3, Symbol.Spade, Symbol.Spade, true)]
+        [InlineData(5, 4, Symbol.Heart, Symbol.Heart, true)]
+        public void CheckTwoDengTest(int cardNo1, int cardNo2, string cardNo1Symbol, string cardNo2Symbol, bool expected)
+        {
+            var sut = new Pokdeng();
+            var result = sut.IsTwoDengCheck(cardNo1, cardNo2, cardNo1Symbol, cardNo2Symbol);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory(DisplayName = "ThreeDeng")]
+        [InlineData(4, 1, 2, Symbol.Club, Symbol.Club, Symbol.Club, true)]
+        [InlineData(3, 1, 3, Symbol.Club, Symbol.Heart, Symbol.Heart, false)]
+        [InlineData(10, 2, 10, Symbol.Heart, Symbol.Heart, Symbol.Heart, true)]
+        public void CheckThreeDengTest(int cardNo1, int cardNo2, int cardNo3, string cardNo1Symbol, string cardNo2Symbol, string cardNo3Symbol, bool expected)
+        {
+            var sut = new Pokdeng();
+            var result = sut.IsThreeDengCheck(cardNo1, cardNo2,cardNo3, cardNo1Symbol, cardNo2Symbol,cardNo3Symbol);
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
--- src/CodeBasic/Pokdeng.cs
+++ src/CodeBasic/Pokdeng.cs
@@ -7,16 +7,79 @@ namespace CodeBasic
     public class Pokdeng
     {
         public int PlayerBalance { get; set; }
-
+        public class Symbol
+        {
+            public const string Club = "Club";
+            public const string Diamond = "Diamond";
+            public const string Heart = "Heart";
+            public const string Spade = "Spade";
+        }
         // Club, Diamond, Heart, Spade (case sensitive)
         public void CheckGameResult(
             int betAmount,
-            int p1CardNo1, int p1CardNo2, int p1CardNo3, 
+            int p1CardNo1, int p1CardNo2, int p1CardNo3,
             string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3,
             int p2CardNo1, int p2CardNo2, int p2CardNo3,
             string p2CardSymbol1, string p2CardSymbol2, string p2CardSymbol3)
         {
-            throw new NotImplementedException();
+
+        }
+        public int SumCard(int cardNo1, int cardNo2, int cardNo3)
+        {
+            if (cardNo1 >= 10)
+            {
+                cardNo1 = 0;
+            }
+           if (cardNo2 >= 10)
+            {
+                cardNo2 = 0;
+
+            }
+            if (cardNo3 >= 10)
+            {
+                cardNo3 = 0;
+
+            }
+            return (cardNo1 + cardNo2 + cardNo3) % 10;
+        }
+        //เช็คป็อก
+        public bool IsPokCheck(int cardNo1, int cardNo2, int cardNo3)
+        {
+            var Card1 = cardNo1;
+            var Card2 = cardNo2;
+            var Card3 = cardNo3;
+            if (Card1 >= 10)
+            {
+                Card1 = 0;
+            }
+            else if (Card2 >= 10)
+            {
+                Card2 = 0;
+            }
+
+            if ((Card1 + Card2 == 8 || Card1 + Card2 == 9) && Card3 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsTwoDengCheck(int cardNo1, int cardNo2, string cardNo1Symbol, string cardNo2Symbol)
+        {
+            if (cardNo1 == cardNo2 || cardNo1Symbol == cardNo2Symbol)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsThreeDengCheck(int cardNo1, int cardNo2, int cardNo3, string cardNo1Symbol, string cardNo2Symbol, string cardNo3Symbol)
+        {
+            if (cardNo1Symbol == cardNo2Symbol && cardNo2Symbol == cardNo3Symbol)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
