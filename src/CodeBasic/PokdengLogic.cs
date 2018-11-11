40f1a39cf0855da2031f76350d2a8b4477c64b3c
powerofwind <powerofwind10@gmail.com>
2018-11-11 22:09:53 +0700

    Normal cases passed

---
+++ src/CodeBasic/PokdengLogic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeBasic
+{
+    public class PokdengLogic
+    {
+        public int PlayerBalance { get; set; }
+
+        // Club, Diamond, Heart, Spade (case sensitive)
+        public void CheckGameResult(
+            int betAmount,
+            int p1CardNo1, int p1CardNo2, int p1CardNo3,
+            string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3,
+            int p2CardNo1, int p2CardNo2, int p2CardNo3,
+            string p2CardSymbol1, string p2CardSymbol2, string p2CardSymbol3)
+        {
+            var DealerPoint = p1CardNo1 + p1CardNo2 + p1CardNo3;
+            var PlayerPoint = p2CardNo1 + p2CardNo2 + p2CardNo3;
+
+            var isgamedraw = DealerPoint == PlayerPoint;
+            if (isgamedraw) return;
+
+            var playerwin = PlayerPoint > DealerPoint;
+            if (playerwin)
+            {
+                PlayerBalance += betAmount;
+            }
+            else
+            {
+                PlayerBalance -= betAmount;
+            }
+
+
+        }
+    }
+}
