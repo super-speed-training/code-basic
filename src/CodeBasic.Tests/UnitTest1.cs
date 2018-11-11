75c41542c25041a072131ccc9c7da7f127bde4f3
aphaphon <pui.aphaphon@gmail.com>
2018-11-11 21:20:43 +0700

    Refactor playerwin-lose
    Signed-off-by: aphaphon <pui.aphaphon@gmail.com>

--- src/CodeBasic.Tests/UnitTest1.cs
+++ src/CodeBasic.Tests/UnitTest1.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Xunit;
+﻿using Xunit;
 
 namespace CodeBasic.Tests
 {
@@ -11,21 +10,25 @@ namespace CodeBasic.Tests
         //ผู้เล่นและเจ้ามือ เสมอกัน ทั้งสองฝ่ายไม่เสียอะไร
         // Alternative cases
         // Exception cases
-        [Fact(DisplayName = "ผู้เล่นชนะ เจ้ามือ ผู้เล่นได้เงินเท่าที่ลงเงิน")]
-        public void CheckGameResultIsPlayerWin()
+        [Theory(DisplayName = "ผู้เล่นชนะเจ้ามือ ผู้เล่นได้เงินเท่าที่ลงเงิน")]
+        [InlineData(100, 5, 2, 0, "Club", "Club", "", 7, 2, 0, "Club", "Club", "", 500, 600)]
+        [InlineData(500, 3, 2, 0, "Club", "Club", "", 5, 2, 0, "Club", "Club", "", 1000, 1500)]
+        public void CheckGameResultIsPlayerWin(int bet, int p1CN1, int p1CN2, int p1CN3, string p1CS1, string p1CS2, string p1CS3, int p2CN1, int p2CN2, int p2CN3, string p2CS1, string p2CS2, string p2CS3, int balance, int expectedBalance)
         {
             var sut = new Pokdeng();
-            sut.PlayerBalance = 500;
-            sut.CheckGameResult(100,5,2,0, "Club", "Club","",7,2,0, "Club", "Club","");
-            Assert.Equal(600,sut.PlayerBalance);
+            sut.PlayerBalance = balance;
+            sut.CheckGameResult(bet, p1CN1, p1CN2, p1CN3, p1CS1, p1CS2, p1CS3, p2CN1, p2CN2, p2CN3, p2CS1, p2CS2, p2CS3);
+            Assert.Equal(expectedBalance, sut.PlayerBalance);
         }
-        [Fact(DisplayName = "ผู้เล่นแพ้เจ้ามือ ชนะ ผู้เล่นเสียเงินที่ลงไป")]
-        public void CheckGameResultIsHostrWin()
+
+        [Theory(DisplayName = "ผู้เล่นแพ้เจ้ามือ ชนะ ผู้เล่นเสียเงินที่ลงไป")]
+        [InlineData(100, 7, 2, 0, "Club", "Club", "", 5, 2, 0, "Club", "Club", "", 500, 400)]
+        public void CheckGameResultIsHostrWin(int bet, int p1CN1, int p1CN2, int p1CN3, string p1CS1, string p1CS2, string p1CS3, int p2CN1, int p2CN2, int p2CN3, string p2CS1, string p2CS2, string p2CS3, int balance, int expectedBalance)
         {
             var sut = new Pokdeng();
-            sut.PlayerBalance = 500;
-            sut.CheckGameResult(100, 7, 2, 0, "Club", "Club", "", 5, 2, 0, "Club", "Club", "");
-            Assert.Equal(400, sut.PlayerBalance);
+            sut.PlayerBalance = balance;
+            sut.CheckGameResult(bet, p1CN1, p1CN2, p1CN3, p1CS1, p1CS2, p1CS3, p2CN1, p2CN2, p2CN3, p2CS1, p2CS2, p2CS3);
+            Assert.Equal(expectedBalance, sut.PlayerBalance);
         }
     }
 }
--- src/CodeBasic/Pokdeng.cs
+++ src/CodeBasic/Pokdeng.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CodeBasic
+﻿namespace CodeBasic
 {
     public class Pokdeng
     {
@@ -11,19 +7,19 @@ namespace CodeBasic
         // Club, Diamond, Heart, Spade (case sensitive)
         public void CheckGameResult(
             int betAmount,
-            int p1CardNo1, int p1CardNo2, int p1CardNo3, 
+            int p1CardNo1, int p1CardNo2, int p1CardNo3,
             string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3,
             int p2CardNo1, int p2CardNo2, int p2CardNo3,
             string p2CardSymbol1, string p2CardSymbol2, string p2CardSymbol3)
         {
-            var HostPoint = p1CardNo1 + p1CardNo2 + p1CardNo3;
-            var PlayerPoint = p2CardNo1 + p2CardNo2 + p2CardNo3;
-            var Winer = PlayerPoint > HostPoint;
-            if (Winer)
+            var hostPoint = p1CardNo1 + p1CardNo2 + p1CardNo3;
+            var playerPoint = p2CardNo1 + p2CardNo2 + p2CardNo3;
+            var isPlayerWin = playerPoint > hostPoint;
+            if (isPlayerWin)
             {
                 PlayerBalance += betAmount;
             }
-            else if (HostPoint > PlayerPoint)
+            else
             {
                 PlayerBalance -= betAmount;
             }
