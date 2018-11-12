5d95153f82914fd56286161227d4d3c1c0232cf2
tatae <tachapong999.gmail.com>
2018-11-12 21:32:54 +0700

    /test/P1 = P2

--- src/CodeBasic/Pokdeng.cs
+++ src/CodeBasic/Pokdeng.cs
@@ -7,24 +7,59 @@ namespace CodeBasic
     public class Pokdeng
     {
         public int PlayerBalance { get; set; }
-        
 
         // Club, Diamond, Heart, Spade (case sensitive)
-        public int CheckGameResult(
+        public void CheckGameResult(
             int betAmount,
             int p1CardNo1, int p1CardNo2, int p1CardNo3,
             string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3,
             int p2CardNo1, int p2CardNo2, int p2CardNo3,
             string p2CardSymbol1, string p2CardSymbol2, string p2CardSymbol3)
         {
-            int SumCardP1 = p1CardNo1+p1CardNo2;
-            int SumCardP2 = p2CardNo1+p2CardNo2;
+                      
+           // ถ้าป๊อกจะได้ไพ่สองใบ
+           int SumTwoP1 =  p1CardNo1 + p1CardNo2;
+           int SumTwoP2 = p2CardNo1 + p2CardNo2;
 
-            if(SumCardP1 == 8 || SumCardP1 == 9 && SumCardP1 > SumCardP2)
-            {
-                return PlayerBalance+betAmount;
-            }
-           return 0;
+           // ถ้าไม่ป๊อกจะได้ไพ่ สองหรือสามใบ
+           int SumthreeP1 = p1CardNo1 + p1CardNo2 +  p1CardNo3;
+           int SumthreeP2 = p2CardNo1 +  p2CardNo2 +  p2CardNo3;
+
+           // P1 ชนะจะได้เงินหนึ่งเท่าของที่ลง ในกรณีที่ได้ 9 หรือ 8
+           var SumP1Pok  =  SumTwoP1 > SumTwoP2 ;
+            // P2 ชนะจะได้เงินหนึ่งเท่าของที่ลง ในกรณีที่ได้ 9 หรือ 8
+           var SumP2Pok  =  SumTwoP2 > SumTwoP1;
+
+           //  P1 ชนะจะได้เงินหนึ่งเท่าของที่ลง ในกรณีที่ได้การ์ด 3 ใบ
+           var SumP1Win = SumthreeP1 > SumthreeP2;
+           // P2 ชนะจะได้เงินหนึ่งเท่าของที่ลง ในกรณีที่ SumthreeP2 > SumthreeP1
+           var SumP2Win = SumthreeP2 > SumthreeP1;
+
+           //ในกรณีที่ P1 ชนะ และได้เงินสองเท่า
+            var SumTwoSymbolP1 = p1CardSymbol1 == p1CardSymbol2 && SumTwoP1 > SumTwoP2;
+            var SumTwoCardP1 = p1CardNo1 == p1CardNo2 && SumTwoP1 > SumTwoP2;
+            // ในกรณีที่ P2 ชนะ และได้เงินสองเท่า
+            var SumTwoSymbolP2 = p2CardSymbol1 == p2CardSymbol2 && SumTwoP1 < SumTwoP2;
+            var SumTwoCardP2 = p1CardNo1 == p1CardNo2 && SumTwoP1 < SumTwoP2;
+
+            // ในกรณีที่ P1 ชนะ และได้เงินสามเท่า
+            var SumThreeSymbolP1 = p1CardSymbol1 == p1CardSymbol2 &&  p1CardSymbol2 ==  p1CardSymbol3
+            && SumthreeP1 > SumthreeP2;
+            var SumThreeCardP1 = p1CardNo1 == p1CardNo2 && p1CardNo2 ==  p1CardNo3
+            && SumthreeP1 > SumthreeP2;
+
+            // ในกรณีที่ P2 ชนะ และได้เงินสามเท่า
+            var SumThreeSymbolP2 = p2CardSymbol1 == p2CardSymbol2 &&  p2CardSymbol2 == p2CardSymbol3
+            && SumthreeP1 < SumthreeP2;
+            var SumThreeCardP2 = p1CardNo1 == p1CardNo2  && p1CardNo2 ==  p1CardNo3
+            && SumthreeP1 < SumthreeP2;
+
+            // ถ้าเจ้ามือมีไพ่สองใบ และ ลูกขามีใบสามใบ
+            if (SumTwoP1 == SumTwoP2)
+           {
+               
+           }
+            
         }
     }
 }
