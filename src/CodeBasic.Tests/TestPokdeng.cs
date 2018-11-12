eec2f6873ca0af52e7ac22b0f1c3cbee3a69d511
apiched1 <apiched_oh@hotmail.com>
2018-11-12 20:46:14 +0700

    pass all test

--- src/CodeBasic.Tests/TestPokdeng.cs
+++ src/CodeBasic.Tests/TestPokdeng.cs
@@ -43,7 +43,7 @@ namespace CodeBasic.Tests
             Assert.Equal(expectedBalance, sut.PlayerBalance);
         }
 
-        [Theory(DisplayName = "ผู้เล่นชนะด้วยป๊อก 9")]
+        [Theory(DisplayName = "ผู้เล่นชนะด้วยป๊อก 9 และได้เงิน x 2")]
         [InlineData(100, 1, 1, Club, Club, 8, 1, Club, Club, 1000, 1100)]
         [InlineData(100, 3, 2, Club, Club, 7, 2, Club, Club, 1000, 1100)]
         [InlineData(100, 1, 4, Club, Club, 5, 4, Club, Club, 1000, 1100)]
@@ -55,7 +55,7 @@ namespace CodeBasic.Tests
             Assert.Equal(expectedBalance, sut.PlayerBalance);
         }
 
-        [Theory(DisplayName = "ผู้เล่นชนะด้วยป๊อก 8")]
+        [Theory(DisplayName = "ผู้เล่นชนะด้วยป๊อก 8 และได้เงิน x 2")]
         [InlineData(100, 1, 1, Club, Club, 7, 1, Club, Club, 1000, 1100)]
         [InlineData(100, 3, 2, Club, Club, 7, 1, Club, Club, 1000, 1100)]
         [InlineData(100, 1, 4, Club, Club, 5, 3, Club, Club, 1000, 1100)]
@@ -67,10 +67,10 @@ namespace CodeBasic.Tests
             Assert.Equal(expectedBalance, sut.PlayerBalance);
         }
 
-        [Theory(DisplayName = "เจ้ามือชนะด้วยป๊อก 9")]
-        [InlineData(100, 7, 2, Club, Club, 1, 1, Club, Club, 1000, 900)]
-        [InlineData(100, 8, 1, Club, Club, 3, 1, Club, Club, 1000, 900)]
-        [InlineData(100, 5, 4, Club, Club, 2, 3, Club, Club, 1000, 900)]
+        [Theory(DisplayName = "เจ้ามือชนะด้วยป๊อก 9 และได้เงิน x 2")]
+        [InlineData(100, 7, 2, Club, Club, 1, 1, Club, Club, 1000, 1200)]
+        [InlineData(100, 8, 1, Club, Club, 3, 1, Club, Club, 1000, 1200)]
+        [InlineData(100, 5, 4, Club, Club, 2, 3, Club, Club, 1000, 1200)]
         public void DealerWinWithPok9(int bet, int p1cn1, int p1cn2, string p1cs1, string p1cs2, int p2cn1, int p2cn2, string p2cs1, string p2cs2, int balance, int expectedBalance)
         {
             var sut = new Pokdeng { PlayerBalance = balance };
@@ -79,19 +79,19 @@ namespace CodeBasic.Tests
             Assert.Equal(expectedBalance, sut.PlayerBalance);
         }
 
-        //  [Theory(DisplayName = "เจ้ามือชนะด้วยป๊อก 8")]
-        // [InlineData(100, 7, 1, Club, Club, 1, 1, Club, Club, 1000, 1100)]
-        // [InlineData(100, 6, 2, Club, Club, 3, 1, Club, Club, 1000, 1100)]
-        // [InlineData(100, 5, 3, Club, Club, 2, 3, Club, Club, 1000, 1100)]
-        // public void DealerWinWithPok8(int bet, int p1cn1, int p1cn2, string p1cs1, string p1cs2, int p2cn1, int p2cn2, string p2cs1, string p2cs2, int balance, int expectedBalance)
-        // {
-        //     var sut = new Pokdeng { PlayerBalance = balance };
-        //     sut.PlayerBalance = balance;
-        //     sut.CheckGameResult(bet, p1cn1, p1cn2, 0, p1cs1, p1cs2, string.Empty, p2cn1, p2cn2, 0, p2cs1, p2cs2, string.Empty);
-        //     Assert.Equal(expectedBalance, sut.PlayerBalance);
-        // }
+        [Theory(DisplayName = "เจ้ามือชนะด้วยป๊อก 8 และได้เงิน x 2")]
+        [InlineData(100, 7, 1, Club, Club, 1, 1, Club, Club, 1000, 1200)]
+        [InlineData(100, 6, 2, Club, Club, 3, 1, Club, Club, 1000, 1200)]
+        [InlineData(100, 5, 3, Club, Club, 2, 3, Club, Club, 1000, 1200)]
+        public void DealerWinWithPok8(int bet, int p1cn1, int p1cn2, string p1cs1, string p1cs2, int p2cn1, int p2cn2, string p2cs1, string p2cs2, int balance, int expectedBalance)
+        {
+            var sut = new Pokdeng { PlayerBalance = balance };
+            sut.PlayerBalance = balance;
+            sut.CheckGameResult(bet, p1cn1, p1cn2, 0, p1cs1, p1cs2, string.Empty, p2cn1, p2cn2, 0, p2cs1, p2cs2, string.Empty);
+            Assert.Equal(expectedBalance, sut.PlayerBalance);
+        }
 
-        
+
 
         // Normal cases
         // Alternative cases
